6fed6146ff944be48d1576f08feddc8744e92272
sunilrao91 <sunilrao91@gmail.com>
2015-07-08 12:50:57 +0530

    Refactored code

--- AutoTask/ThisAddIn.cs
+++ AutoTask/ThisAddIn.cs
@@ -1,24 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
 
 namespace AutoTask
 {
     using System.Windows.Forms;
-
+    
     public partial class ThisAddIn
     {
         private Office.CommandBar menuBar;
         private Office.CommandBarPopup newMenuBar;
         private Office.CommandBarButton buttonOne;
-        private string menuTag = "AUniqueTag";
+        private readonly string menuTag = "AutoTask";
         private Outlook.NameSpace outlookNamespace;
         private Outlook.MAPIFolder inbox;
-        private Outlook.Items items;
+        private Outlook.Items outlookItems;
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
@@ -26,34 +22,28 @@ namespace AutoTask
             AddMenuBar();
 
             outlookNamespace = this.Application.GetNamespace("MAPI");
-            inbox = outlookNamespace.GetDefaultFolder(
-            Microsoft.Office.Interop.Outlook.
-            OlDefaultFolders.olFolderInbox);
+            inbox = outlookNamespace.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
 
-            items = inbox.Items;
-            items.ItemAdd +=
-                new Outlook.ItemsEvents_ItemAddEventHandler(items_ItemAdd);
+            outlookItems = inbox.Items;
+            outlookItems.ItemAdd += new Outlook.ItemsEvents_ItemAddEventHandler(items_ItemAdd);
         }
 
-        void items_ItemAdd(object Item)
+        void items_ItemAdd(object item)
         {
-            string filter = "USED CARS";
-            Outlook.MailItem mail = (Outlook.MailItem)Item;
-            if (Item != null)
+            var mail = (Outlook.MailItem)item;
+            
+            if (mail.Body.Contains("Sunil"))
             {
-                if (mail.Body.Contains("Sunil"))
-                {
-                    Outlook.TaskItem oTask = this.Application.CreateItem(Outlook.OlItemType.olTaskItem);
-                    oTask.Subject = "This is my task subject";
-                    oTask.DueDate = Convert.ToDateTime("06/25/2011");
-                    oTask.StartDate = Convert.ToDateTime("06/20/2011");
-                    oTask.ReminderSet = true;
-                    oTask.ReminderTime = Convert.ToDateTime("06/28/2006 02:40:00 PM");
-                    oTask.Body = mail.Body;
-                    oTask.SchedulePlusPriority = "High";
-                    oTask.Status = Microsoft.Office.Interop.Outlook.OlTaskStatus.olTaskInProgress;
-                    oTask.Save();
-                }
+                Outlook.TaskItem oTask = this.Application.CreateItem(Outlook.OlItemType.olTaskItem);
+                oTask.Subject = mail.Subject;
+                oTask.DueDate = mail.ReceivedTime.AddHours(6);
+                oTask.StartDate = mail.ReceivedTime;
+                oTask.ReminderSet = true;
+                oTask.ReminderTime = mail.ReceivedTime.AddMinutes(30);
+                oTask.Body = mail.Body;
+                oTask.SchedulePlusPriority = "High";
+                oTask.Status = Outlook.OlTaskStatus.olTaskInProgress;
+                oTask.Save();
             }
         }
 
@@ -71,17 +61,13 @@ namespace AutoTask
                     missing, missing, false);
                 if (newMenuBar != null)
                 {
-                    newMenuBar.Caption = "See New Icon";
+                    newMenuBar.Caption = "AutoTask";
                     newMenuBar.Tag = menuTag;
-                    buttonOne = (Office.CommandBarButton)
-                        newMenuBar.Controls.
-                    Add(Office.MsoControlType.msoControlButton, System.
-                        Type.Missing, System.Type.Missing, 1, true);
-                    buttonOne.Style = Office.MsoButtonStyle.
-                        msoButtonIconAndCaption;
-                    buttonOne.Caption = "New Icon";
+                    buttonOne = (Office.CommandBarButton)newMenuBar.Controls.Add(Office.MsoControlType.msoControlButton, Type.Missing, Type.Missing, 1, true);
+                    buttonOne.Style = Office.MsoButtonStyle.msoButtonIconAndCaption;
+                    buttonOne.Caption = "AutoTask";
                     buttonOne.FaceId = 65;
-                    buttonOne.Tag = "c123";
+                    buttonOne.Tag = "AutoTask";
                     buttonOne.Picture = getImage();
                     newMenuBar.Visible = true;
                 }
@@ -100,7 +86,7 @@ namespace AutoTask
                 System.Drawing.Icon newIcon =
                     Properties.Resources.Icon1;
 
-                ImageList newImageList = new ImageList();
+                var newImageList = new ImageList();
                 newImageList.Images.Add(newIcon);
                 tempImage = ConvertImage.Convert(newImageList.Images[0]);
             }
@@ -116,10 +102,8 @@ namespace AutoTask
             // If the menu already exists, remove it. 
             try
             {
-                Office.CommandBarPopup foundMenu = (Office.CommandBarPopup)
-                    this.Application.ActiveExplorer().CommandBars.ActiveMenuBar.
-                    FindControl(Office.MsoControlType.msoControlPopup,
-                    System.Type.Missing, menuTag, true, true);
+                var foundMenu = (Office.CommandBarPopup) this.Application.ActiveExplorer().CommandBars.ActiveMenuBar.
+                    FindControl(Office.MsoControlType.msoControlPopup,System.Type.Missing, menuTag, true, true);
                 if (foundMenu != null)
                 {
                     foundMenu.Delete(true);
@@ -148,17 +132,13 @@ namespace AutoTask
 
     sealed public class ConvertImage : System.Windows.Forms.AxHost
     {
-        private ConvertImage()
-            : base(null)
+        private ConvertImage() : base(null)
         {
         }
 
-        public static stdole.IPictureDisp Convert
-            (System.Drawing.Image image)
+        public static stdole.IPictureDisp Convert(System.Drawing.Image image)
         {
-            return (stdole.IPictureDisp)System.
-                Windows.Forms.AxHost
-                .GetIPictureDispFromPicture(image);
+            return (stdole.IPictureDisp)System.Windows.Forms.AxHost.GetIPictureDispFromPicture(image);
         }
     }
 }
